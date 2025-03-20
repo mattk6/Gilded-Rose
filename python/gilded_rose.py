c0e897c9fcbca3c1ad8ecf5f0a88cce2ede0c5e8
Matthew Kruse <kruse.matthew2@gmail.com>
2025-03-20 18:00:07 -0500

    tests run now

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -11,8 +11,7 @@ class GildedRose(object):
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
+            elif item.quality < 50:
                     item.quality = item.quality + 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
