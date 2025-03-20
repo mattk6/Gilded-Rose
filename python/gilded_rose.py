57ac76240b0a0b7c0eeb21b3330780418f5802e3
Matthew Kruse <kruse.matthew2@gmail.com>
2025-03-20 18:42:01 -0500

    readability fix in progress

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,36 +1,46 @@
 # -*- coding: utf-8 -*-
 
 class GildedRose(object):
-
+    
+    AGED_BRIE = "Aged Brie"
+    BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
+    
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
             
-            if item.name not in ("Aged Brie", "Backstage passes to a TAFKAL80ETC concert"):
-                if item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros":
+            # anything that's not Aged Brie logic and backstage passes logic
+            if item.name not in (self.AGED_BRIE, self.BACKSTAGE_PASSES):
+                # decrement if above 0 and not Sulfuras
+                if item.quality > 0 and item.name != self.SULFURAS:
                         item.quality -= 1
-            
+
+
+            # anything that is Aged Brie or backstage passes logic, still could be Sulfuras
             elif item.quality < 50:
                     item.quality += 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        
-                        if item.sell_in < 11 and item.quality < 50:
+                    
+                    # Backstage passes logic
+                    if item.name == self.BACKSTAGE_PASSES:
+                        # if item.sell_in < 11 add 1 to quality
+                        if item.sell_in < 11:
                                 item.quality += 1
-                        
-                        if item.sell_in < 6 and item.quality < 50:
+                        # if item.sell_in < 6 add 1 to quality again
+                        if item.sell_in < 6:
                                 item.quality += 1
             
             
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != self.SULFURAS:
                 item.sell_in -= 1
             
             
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros":
+                if item.name != self.AGED_BRIE:
+                    if item.name != self.BACKSTAGE_PASSES:
+                        if item.quality > 0 and item.name != self.SULFURAS:
                                 item.quality -= 1
                     else:
                         item.quality = 0
